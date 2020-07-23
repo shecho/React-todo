e3c51f535398c25aadc4e76b559159e4acc8742d
shecho <shechovala@gmail.com>
2020-07-23 01:02:25 -0500

    Resgister request working prpertly

--- src/components/register/register2.jsx
+++ src/components/register/register2.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./register2.css";
 
 export default function Register2() {
+  const [user, setUser] = useState({});
+
+  let handleInput = (e) => {
+    e.preventDefault();
+
+    setUser({ ...user, [e.target.name]: e.target.value });
+    // console.log(user);
+  };
+
+  let registerUser = async (e) => {
+    e.preventDefault();
+    // console.log(JSON.stringify(user));
+    let url = `https://academlo-todolist.herokuapp.com/register`;
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(user),
+    });
+    let res = await response.json();
+    console.log(res);
+  };
+
   return (
     <div className="">
-      <form action="index.html" className="form-box animated fadeInUp">
+      <form
+        onInput={handleInput}
+        onSubmit={(e) => registerUser(e)}
+        className="form-box animated fadeInUp"
+      >
         <h1 className="form-title">SignUp</h1>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input name="name" type="text" placeholder="Name" />
+        <input name="lastname" type="text" placeholder="Lastname" />
+        <input name="email" type="email" placeholder="Email" />
+        <input name="password" type="password" placeholder="Password" />
         <button type="submit">
           <i className="fa fa-user-plus px-2" aria-hidden="true"></i>
           Register
