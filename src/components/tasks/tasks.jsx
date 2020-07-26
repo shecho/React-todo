df0c5097b5a046fdbb7ce49d788f65c544ebb7c5
shecho <shechovala@gmail.com>
2020-07-26 00:45:29 -0500

    layout fixes

--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -1,27 +1,64 @@
 import React, { useState, useEffect } from "react";
-import Task from '../task/task';
+import Task from "../task/task";
+import './tasks.css'
 const Tasks = (props) => {
-  const [tasks, setTasks] = useState()
+  const [tasks, setTasks] = useState();
 
-    useEffect(() => {
-    getTasks()
+  useEffect(() => {
+    getTasks();
   }, []);
-
 
   const getTasks = async () => {
     let url = `https://academlo-todolist.herokuapp.com/tasks`;
     // console.log(url);
     let response = await fetch(url);
     console.log(response);
-
   };
+
+  const addTask = async (e) => {
+    console.log(e);
+  };
+  const deleteTask = async (e) => {
+    console.log(e);
+  };
+  const updateTask = async (e) => {
+    console.log(e);
+  };
+
   return (
-    <div className="row">
-      <Task />
-      <Task />
-      <Task />
-      <Task />
-    </div>
+    <>
+      <div className="container">
+        <div className="headerTasks">
+          <div className="d-flex flex-column">
+            <div className="">
+              <button className="w-100 btn btn-primary btn-large">
+                <i className="h1 fa fa-plus-circle" aria-hidden="true"></i>{" "}
+              </button>
+              <button className="w-100 btn btn-primary btn-large">
+                <i className="h1 fa fa-plus-circle" aria-hidden="true"></i>{" "}
+              </button>
+ 
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="row">
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
